7ad2e1e64835b4a5614e56487c7480a8c683fd98
Paul Sonnentag paul.sonnentag@gmail.com <paul.sonnentag@googlemail.com>
2019-09-24 16:41:35 +0200

    Implement basic box creation

--- src/Main.elm
+++ src/Main.elm
@@ -3,23 +3,124 @@ module Main exposing (main)
 import Browser
 import Element exposing (column, el, px, row)
 import Element.Background as Background
-import Html exposing (div)
-import Html.Attributes exposing (style)
+import Html exposing (Html, div)
+import Html.Attributes exposing (class, classList, style)
+import Html.Events exposing (on, onMouseLeave, onMouseUp)
+import Json.Decode as Decode exposing (Decoder)
 import Theme
 
 
-main =
-    Browser.sandbox { init = 0, update = update, view = view }
+
+-- model
+
+
+type alias Point =
+    { x : Int, y : Int }
+
+
+type MouseAction
+    = None
+    | CreateBox Point Point
+
+
+type alias Model =
+    { mouseAction : MouseAction
+    , boxes : List Box
+    }
+
+
+type alias Box =
+    { top : Int
+    , left : Int
+    , width : Int
+    , height : Int
+    }
+
+
+initialState : Model
+initialState =
+    { mouseAction = None
+    , boxes = []
+    }
+
+
+boxFromCorners : Point -> Point -> Box
+boxFromCorners startPoint endPoint =
+    let
+        left =
+            min startPoint.x endPoint.x
+
+        right =
+            max startPoint.x endPoint.x
+
+        top =
+            min startPoint.y endPoint.y
+
+        bottom =
+            max startPoint.y endPoint.y
+    in
+    { left = left
+    , top = top
+    , width =
+        right - left
+    , height =
+        bottom - top
+    }
+
+
+
+-- update
 
 
 type Msg
     = NoOp
+    | StartCreateBox Point
+    | UpdateMouseAction Point
+    | CompleteMouseAction
 
 
 update msg model =
     case msg of
+        StartCreateBox startPoint ->
+            { model | mouseAction = CreateBox startPoint startPoint }
+
+        UpdateMouseAction endPoint ->
+            case model.mouseAction of
+                CreateBox startPoint _ ->
+                    { model | mouseAction = CreateBox startPoint endPoint }
+
+                None ->
+                    model
+
+        CompleteMouseAction ->
+            case model.mouseAction of
+                CreateBox startPoint endPoint ->
+                    let
+                        newBox =
+                            boxFromCorners startPoint endPoint
+                    in
+                    { model
+                        | mouseAction = None
+                        , boxes =
+                            model.boxes
+                                -- only add box if it is at least 1 x 1 pixels
+                                ++ (if newBox.width > 0 && newBox.height > 0 then
+                                        [ newBox ]
+
+                                    else
+                                        []
+                                   )
+                    }
+
+                None ->
+                    model
+
         NoOp ->
             model
+
+
+
+-- view
 
 
 sidebar =
@@ -31,21 +132,72 @@ sidebar =
         []
 
 
-canvas =
+canvas : List Box -> MouseAction -> Element.Element Msg
+canvas boxes mouseAction =
     el
         [ Element.width Element.fill
         , Element.height Element.fill
         ]
-        (div
-            [ style "width" "100%"
-            , style "height" "100%"
-            , style "background" (Theme.colorToString Theme.subtleDarkColor)
-            ]
-            []
-            |> Element.html
+        (Element.html
+            (div
+                [ class "canvas"
+                , classList [ ( "disable-child-mouse-events", mouseAction /= None ) ]
+                , style "cursor" "crosshair"
+                , on "mousedown" (Decode.map StartCreateBox mousePosition)
+                , on "mousemove" (Decode.map UpdateMouseAction mousePosition)
+                , onMouseLeave CompleteMouseAction
+                , onMouseUp CompleteMouseAction
+                ]
+                (List.map boxView boxes
+                    ++ mouseActionPreview mouseAction
+                )
+            )
         )
 
 
+mouseActionPreview : MouseAction -> List (Html msg)
+mouseActionPreview mouseAction =
+    case mouseAction of
+        CreateBox startPoint endPoint ->
+            let
+                box =
+                    boxFromCorners startPoint endPoint
+            in
+            [ div
+                [ class "createBoxPreview"
+                , style "width" (String.fromInt box.width ++ "px")
+                , style "height" (String.fromInt box.height ++ "px")
+                , style "top" (String.fromInt box.top ++ "px")
+                , style "left" (String.fromInt box.left ++ "px")
+                ]
+                []
+            ]
+
+        None ->
+            []
+
+
+mousePosition : Decoder Point
+mousePosition =
+    Decode.map2 Point
+        (Decode.field "offsetX" Decode.int)
+        (Decode.field "offsetY" Decode.int)
+
+
+boxView : Box -> Html Msg
+boxView { top, left, width, height } =
+    div
+        [ class "box"
+        , style "top" (String.fromInt top ++ "px")
+        , style "left" (String.fromInt left ++ "px")
+        , style "width" (String.fromInt width ++ "px")
+        , style "height" (String.fromInt height ++ "px")
+        , style "background-color" (Theme.colorToString Theme.highlightColor)
+        ]
+        []
+
+
+view : Model -> Html Msg
 view model =
     Element.layout
         []
@@ -54,6 +206,10 @@ view model =
             , Element.height Element.fill
             ]
             [ sidebar
-            , canvas
+            , canvas model.boxes model.mouseAction
             ]
         )
+
+
+main =
+    Browser.sandbox { init = initialState, update = update, view = view }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './styles.css';
 import { Elm } from './Main.elm'
 
 Elm.Main.init({
