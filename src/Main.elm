37861fa077f60199d744d004775a7b5d764812df
Paul Sonnentag paul.sonnentag@gmail.com <paul.sonnentag@googlemail.com>
2019-09-24 12:50:11 +0200

    Add basic elm-ui layout

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,59 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (h1, text)
+import Element exposing (column, el, px, row)
+import Element.Background as Background
+import Html exposing (div)
+import Html.Attributes exposing (style)
+import Theme
+
 
 main =
-  h1 [] [ text "Hello, Elm!" ]
+    Browser.sandbox { init = 0, update = update, view = view }
+
+
+type Msg
+    = NoOp
+
+
+update msg model =
+    case msg of
+        NoOp ->
+            model
+
+
+sidebar =
+    column
+        [ Background.color Theme.subtleLightColor
+        , Element.width (300 |> px)
+        , Element.height Element.fill
+        ]
+        []
+
+
+canvas =
+    el
+        [ Element.width Element.fill
+        , Element.height Element.fill
+        ]
+        (div
+            [ style "width" "100%"
+            , style "height" "100%"
+            , style "background" (Theme.colorToString Theme.subtleDarkColor)
+            ]
+            []
+            |> Element.html
+        )
+
+
+view model =
+    Element.layout
+        []
+        (row
+            [ Element.width Element.fill
+            , Element.height Element.fill
+            ]
+            [ sidebar
+            , canvas
+            ]
+        )
